88094dad8d865874c566cbdfd63b6b832f0d5b04
Stef Gootzen <stefgootzen@gmail.com>
2024-05-26 13:40:29 +0200

    refactor: move all Counter code to component

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,12 +1,44 @@
+import { useCounter } from "../hooks/useCounter";
+
 type CounterProps = {
-  count: number;
+  initialCount: number;
 };
 
-export default function Counter({ count }: CounterProps) {
+export default function Counter({ initialCount = 0 }: CounterProps) {
+  const { count, incrementByOne, isAutomated, toggleAutomate, reset } = useCounter(initialCount);
+
   return (
-    <div>
-      <p className="heading text-6xl font-medium text-center">{count}</p>
-      <p className="text-xl text-gray-500 font-medium text-center">Cookies</p>
-    </div>
+    <>
+      <div>
+        <p className="heading text-6xl font-medium text-center">{count}</p>
+        <p className="text-xl text-gray-500 font-medium text-center">Cookies</p>
+      </div>
+
+      <div className="h-16" />
+
+      <div className="flex items-center justify-center">
+        <div onClick={incrementByOne}>
+          <img height={200} width={200} src="/cookie.svg" alt="Add cookie" />
+        </div>
+      </div>
+
+      <div className="h-16" />
+
+      <div className="flex items-center justify-center">
+        <div
+          onClick={toggleAutomate}
+          className="shadow-md bg-blue-500 py-4 px-6 rounded-lg text-blue-100 font-bold hover:bg-blue-600 hover:underline focus:ring-2 ring-offset-2 ring-blue-600 focus:outline-none"
+        >
+          Turn automate {isAutomated ? "off" : "on"}
+        </div>
+        <div className="w-3" />
+        <div
+          onClick={reset}
+          className="border shadow-md bg-white py-4 px-6 rounded-lg text-gray-700 font-bold hover:bg-gray-100 hover:underline focus:ring-2 ring-offset-2 ring-blue-600 focus:outline-none"
+        >
+          Reset
+        </div>
+      </div>
+    </>
   );
 }
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,15 +1,11 @@
 import Head from "next/head";
-import { useCallback, useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { useCounter } from "../hooks/useCounter";
 import Counter from "components/Counter";
 
-/**
- * Improve this code as much as possible and make it work.
- * You're allowed to use online resources.
- */
-export default function Home() {
-  const { count, incrementByOne, isAutomated, toggleAutomate, reset } = useCounter(0);
+const INITIAL_COUNT = 0;
 
+export default function Home() {
   return (
     <>
       <Head>
@@ -26,33 +22,7 @@ export default function Home() {
         <div className="h-4" />
 
         <main id="main">
-          <Counter count={count} />
-
-          <div className="h-16" />
-
-          <div className="flex items-center justify-center">
-            <div onClick={incrementByOne}>
-              <img height={200} width={200} src="/cookie.svg" alt="Add cookie" />
-            </div>
-          </div>
-
-          <div className="h-16" />
-
-          <div className="flex items-center justify-center">
-            <div
-              onClick={toggleAutomate}
-              className="shadow-md bg-blue-500 py-4 px-6 rounded-lg text-blue-100 font-bold hover:bg-blue-600 hover:underline focus:ring-2 ring-offset-2 ring-blue-600 focus:outline-none"
-            >
-              Turn automate {isAutomated ? "off" : "on"}
-            </div>
-            <div className="w-3" />
-            <div
-              onClick={reset}
-              className="border shadow-md bg-white py-4 px-6 rounded-lg text-gray-700 font-bold hover:bg-gray-100 hover:underline focus:ring-2 ring-offset-2 ring-blue-600 focus:outline-none"
-            >
-              Reset
-            </div>
-          </div>
+          <Counter initialCount={INITIAL_COUNT} />
         </main>
       </div>
     </>
